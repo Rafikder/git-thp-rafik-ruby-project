6930441f2a7644a247e7543334ac9bf5a105f90c
Rafikder <derouiche.rafik@gmail.com>
2018-09-29 01:54:46 +0200

    Modification exo_19.rb

--- exo_19.rb
+++ exo_19.rb
@@ -2,7 +2,11 @@ emails = []
 
 i = 1
 while i <= 50 do
-  emails.push("jean.dupont.#{i}@email.fr")
+  if i < 10
+    emails.push("jean.dupont.0#{i}@email.fr")
+  else
+    emails.push("jean.dupont.#{i}@email.fr")
+  end
   i += 1
 end
 
