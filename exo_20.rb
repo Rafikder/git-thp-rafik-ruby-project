469b6b4e712c6db36bfb220e03ae75646bf49767
Rafikder <derouiche.rafik@gmail.com>
2018-09-29 02:03:28 +0200

    Modification exo_20.rb

--- exo_20.rb
+++ exo_20.rb
@@ -2,6 +2,12 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 floor = gets.to_i
 
+if not floor.between?(1,25)
+  puts "Entre 1 et 25 ?"
+  print "> "
+  floor = gets.to_i
+end
+
 puts "Voici la pyramide :"
 i = 1
 while i <= floor do
